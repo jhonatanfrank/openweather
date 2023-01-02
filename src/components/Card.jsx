1d902b2663a724ef1202f1f8ce89b47fc0e2cad7
jhonatanfrank <frank_mch97@hotmail.com>
2023-01-02 11:24:19 -0500

    actualizacion responsiva

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -58,64 +58,67 @@ const Card = ({ loadingData, showData, weather, forecast }) => {
   }
 
   return (
-    <div className="mt-5">
-      {showData === true ? (
-        <div className="container">
-          <div className="card mb-3 mx-auto bg-dark text-light">
-            <div className="row g-0">
-              <div className="col-md-4">
-                <h3 className="card-title">{weather.name}</h3>
-                <p className="card-date">{date}</p>
-                <h1 className="card-temp">
-                  {(weather.main.temp - 273.15).toFixed(1)}ºC
-                </h1>
-                <p className="card-desc">
-                  <img src={iconUrl} alt="icon" />
-                  {weather.weather[0].description}
-                </p>
+    <>
+      <div className="mt-5">
+        {showData === true ? (
+          <>
+            <div class="container-clima">
+              <h1>{weather.name} - {date}</h1>
+              <h2>
+                {(weather.main.temp - 273.15).toFixed(1)}ºC
+              </h2>
+              <img src={iconUrl} alt="icon" />
+              <h1>
+                {weather.weather[0].description}
+              </h1>
+            </div>
+            <div class="container-clima">
+
+              <div>
                 <img
                   src="https://images.pexels.com/photos/10817264/pexels-photo-10817264.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                  className="img-fluid rounded-start"
                   alt="..."
+                  className="imagen-clima"
                 />
               </div>
-              <div className="col-md-8">
-                <div className="card-body text-start mt-2">
-                  <h5 className="card-text">
+
+
+              <div>
+                <div>
+                  <h5>
                     Temperatura máxima:{" "}
                     {(weather.main.temp_max - 273.15).toFixed(1)}ºC
                   </h5>
-                  <h5 className="card-text">
+                  <h5>
                     Temperatura mínima:{" "}
                     {(weather.main.temp_min - 273.15).toFixed(1)}ºC
                   </h5>
-                  <h5 className="card-text">
+                  <h5>
                     sensación térmica:{" "}
                     {(weather.main.feels_like - 273.15).toFixed(1)}ºC
                   </h5>
-                  <h5 className="card-text">
+                  <h5>
                     Humedad: {weather.main.humidity}%
                   </h5>
-                  <h5 className="card-text">
+                  <h5>
                     Velocidad del viento: {weather.wind.speed}m/s
                   </h5>
                 </div>
                 <hr />
-
-                <div className="row mt-4">
-                  <div className="col">
+                <div class="container-clima">
+                  <div>
                     <p>{forecastDate3}h</p>
-                    <p className="description">
+                    <p>
                       <img src={iconUrl3} alt="icon" />
                       {forecast.list[1].weather[0].description}
                     </p>
-                    <p className="temp">
+                    <p>
                       {(forecast.list[1].main.temp - 273.15).toFixed(1)}ºC
                     </p>
                   </div>
-                  <div className="col">
+                  <div>
                     <p>{forecastDate6}h</p>
-                    <p className="description">
+                    <p>
                       <img src={iconUrl6} alt="icon" />
                       {forecast.list[2].weather[0].description}
                     </p>
@@ -123,7 +126,7 @@ const Card = ({ loadingData, showData, weather, forecast }) => {
                       {(forecast.list[2].main.temp - 273.15).toFixed(1)}ºC
                     </p>
                   </div>
-                  <div className="col">
+                  <div>
                     <p>{forecastDate9}h</p>
                     <p className="description">
                       <img src={iconUrl9} alt="icon" />
@@ -135,14 +138,15 @@ const Card = ({ loadingData, showData, weather, forecast }) => {
                   </div>
                 </div>
               </div>
+
             </div>
-          </div>
-        </div>
-      ) : (
-        <h2 className="text-light">Sin datos</h2>
-      )}
-    </div>
+          </>
+        ) : (
+          <h2 className="text-light">Sin datos</h2>
+        )}
+      </div>
+    </>
   );
-};
+}
 
 export default Card;
